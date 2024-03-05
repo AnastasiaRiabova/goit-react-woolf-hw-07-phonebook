532a86b44465994d3e0bc57ba1c75a75eed54639
AnastasiaRiabova <riabova321@gmail.com>
2024-03-05 13:47:27 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+import classes from './App.module.css';
+import { getContacts } from '../redux/selectors';
+
+const App = () => {
+  const contacts = useSelector(getContacts);
+
+  return (
+    <div className={classes.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {contacts.length > 0 ? (
+        <>
+          <Filter />
+          <ContactList />
+        </>
+      ) : (
+        'No contacts added'
+      )}
+    </div>
+  );
+};
+
+export default App;
